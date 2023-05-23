c34ea4902e1617d2976e0e5f42acbf1691e540c6
Matheus <maahgomes.r@gmail.com>
2023-05-23 17:33:30 -0300

    adicionando exercicios sobre classes e objetos

---
+++ 05 - objetos/exercicio02_objetos.js
@@ -0,0 +1,45 @@
+/*
+    2)Crie uma classe para representar pessoas.
+    Para cada pessoa teremos os atributos nome, peso e altura.
+    As pessoa devem ter a capacidade de dizer o valor de seu IMC (peso / (altura*altura));
+    Instancie uam pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer
+    o valor do seu IMC;
+*/
+
+class Pessoa {
+    nome;
+    peso;
+    altura;
+
+    constructor(nome, peso, altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    calcularImc() {
+        return this.peso / Math.pow(this.altura, 2);
+    }
+
+    classificarImc() {
+        const imc = this.calcularImc();
+
+        if (imc < 18.5) {
+            return 'Abaixo do peso';
+        } else if (imc <= 25) {
+            return 'Peso normal';
+        } else if (imc <= 30) {
+            return 'Acima do peso';
+        } else if (imc <= 40) {
+            return 'Obeso';
+        } else {
+            return 'Obesidade grave';
+        }
+
+    }
+
+}
+
+const jose = new Pessoa('José', 70, 1.75);
+
+console.log(jose.calcularImc() + ' --> ' + jose.classificarImc());
