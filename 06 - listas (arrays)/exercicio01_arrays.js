e858af573a5a24718525e979d4b1585b7f2a8d4f
Matheus <maahgomes.r@gmail.com>
2023-05-25 16:12:45 -0300

    Adicao dos exercicios de array

---
+++ 06 - listas (arrays)/exercicio01_arrays.js
@@ -0,0 +1,14 @@
+//  1) Crie um programa que dado um número imprima a sua tabuada.
+
+const numero = 2;
+
+for (let i = 1; i <= 10; i++) {
+    let tabuada = numero * i;
+    console.log(numero + ' X ' + i + ' = ' + tabuada);
+
+}
+
+
+
+
+
